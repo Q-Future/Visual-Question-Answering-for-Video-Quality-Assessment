3480562ffc81c85f552eff704a2bd0aa23310877
ZihengJia wansui <116589000+jzhws@users.noreply.github.com>
2025-07-10 16:01:42 +0800

    Delete VQA_benchmark_test directory

--- VQA_benchmark_test/llava-openvision-vllm.py
+++
@@ -1,5 +0,0 @@
-from vllm import ModelRegistry
-from llava.model.language_model.llava_qwen_vllm import LlavaQwenForCausalLM
-ModelRegistry.register_model("LlavaQwenForCausalLM", LlavaQwenForCausalLM)
-import runpy
-runpy.run_module('vllm.entrypoints.openai.api_server', run_name='__main__')
